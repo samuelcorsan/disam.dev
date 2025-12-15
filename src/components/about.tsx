0bcd13ed526904e65567af90328bcb8a820fdc8b
Samuel Corsan <120322525+samuelcorsan@users.noreply.github.com>
2025-12-15 06:11:46 +0100

    chore: upgrade Next.js to version 16.0.10 (fixed CVE)

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -7,12 +7,13 @@ export default function About() {
           I'm a hustler. Every day I'm either learning something new, building
           something, or figuring out what's next. My mission is to retire my
           family, repaying them for everything they've given me, and to build a
-          company that creates a lasting positive impact on humanity.
+          company that creates lasting positive impact on humanity.
         </p>
         <p>
-          When I'm not coding, I'm training or playing football. I also like to
-          debate with friends about current world issues and geopolitics to
-          enrich my knowledge.
+          When I'm not coding, I'm training. I've been practising karate for 7
+          years and currently hold a blue belt. I also play football and enjoy
+          debating current world issues and geopolitics with friends to expand
+          my perspective.
         </p>
       </div>
     </section>
